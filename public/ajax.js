7a42b5e9d38565d6e34d9a79fac543ea196702b5
Ndhossain <hhhssnahiid@gmail.com>
2022-12-15 04:13:47 +0600

    work almost done

--- public/ajax.js
+++ public/ajax.js
@@ -13,43 +13,30 @@ $(function () {
 })
 
 // dropingzone
+Dropzone.autoDiscover = false;
 $(function () {
-    $('#fileupload').fileupload({
-        url: 'http://localhost:5000/upload',
-        dropZone: $('.dropzone'),
-        dataType: 'json',
-        autoUpload: false
-    }).on('drop dragover', function (e) {
-        if(e.originalEvent.type === 'dragover') {
-            $('.dropzone').addClass('afterDrag')
-        };
-    }).on('fileuploadadd', function (e, data) {
-        e.preventDefault();
-        $('.dropzone').removeClass('afterDrag');
-        $('#files').empty();
-        data.submit();
-        console.log(data)
-        data.originalFiles.forEach(file => {
-            if (file){
-                let reader = new FileReader();
-                reader.onload = function(event){
-                    $('#files').append(`<div class="col-md-4"><img class="img-fluid" src="${event.target.result}" alt="upload image" /></div>`);
-                }
-                reader.readAsDataURL(file);
+    $("#my-dropzone").dropzone({
+        url: '/uploadImages',
+        maxFilesize: 5, // MB
+        maxFiles: 10,
+        addRemoveLinks: true,
+        uploadMultiple: true,
+        autoProcessQueue: false,
+        paramName: () => 'images',
+        method: 'post',
+        parallelUploads: 10,
+        init: function () {
+            var wrapperThis = this;
+            $('#formData').submit(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                wrapperThis.processQueue();
+            })
+        },
+        success: function (files, status) {
+            if (status.message === 'upload successful') {
+                document.getElementById('formData').reset()
             }
-        });
-    }).on('fileuploaddone', function (e, data) {
-        console.log(data)
-    }).on('fileuploadprogressall', function (e, data) {
-        console.log(data);
-        // var progress = parseInt(data.loaded / data.total * 100, 10);
-        // $('progress').html(`Compeleted ${progress} %`)
-    })
-});
-
-// submit excercise form
-
-// $("#submitButton").click(function (e) {
-//     e.preventDefault();
-//     // $('#formData').submit()
-// })
+        }
+    });
+})
